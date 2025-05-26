4e4e2b4ead1977cf386028cd35602466a739f8fe
DEV4705 <davegameplay@gmail.com>
2025-05-26 11:08:13 +0700

    LoginManager

--- Assets/Script/LoginManager.cs
+++ Assets/Script/LoginManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+using UnityEngine.Networking;
+using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
+
+
+[System.Serializable]
+public class Data
+{
+    public string Email;
+    public string Password;
+    public string RePassword;
+
+}
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_InputField EmailInput;
+    public TMP_InputField PasswordInput;
+    public TMP_InputField RePasswordInput;
+    public Button Login;
+
     void Start()
     {
-        
+        Login.onClick.AddListener(OnSubmit);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnSubmit()
     {
-        
+        if(PasswordInput != RePasswordInput)
+        {
+            Debug.Log("Wrong Password");
+        }
+
+        Data data = new Data
+        {
+            Email = EmailInput.text,
+            Password = PasswordInput.text,
+            RePassword = RePasswordInput.text,
+        };
+
     }
+
 }
