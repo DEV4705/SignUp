918a4ee573ca4044b28db1b90c21cd0d85efe268
DEV4705 <davegameplay@gmail.com>
2025-05-26 11:44:27 +0700

    ToWebsite

--- Assets/Script/LoginManager.cs
+++ Assets/Script/LoginManager.cs
@@ -7,6 +7,7 @@ using TMPro;
 using UnityEngine.Networking;
 using Unity.VisualScripting;
 using System.Runtime.CompilerServices;
+using static System.Net.WebRequestMethods;
 
 
 [System.Serializable]
@@ -25,14 +26,38 @@ public class LoginManager : MonoBehaviour
     public TMP_InputField RePasswordInput;
     public Button Login;
 
+    private string URL = "https://binusgat.rf.gd/unity-api-test/api/auth/signup.php";
+
     void Start()
     {
         Login.onClick.AddListener(OnSubmit);
     }
 
+    IEnumerator PostRequest(string uri, string json)
+    {
+        UnityWebRequest request = new UnityWebRequest(uri, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("Error: " + request.error);
+            Debug.Log("Full response: " + request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Response: " + request.downloadHandler.text);
+        }
+
+    }
+
     void OnSubmit()
     {
-        if(PasswordInput != RePasswordInput)
+        if(PasswordInput.text != RePasswordInput.text)
         {
             Debug.Log("Wrong Password");
         }
@@ -44,6 +69,11 @@ public class LoginManager : MonoBehaviour
             RePassword = RePasswordInput.text,
         };
 
+        string jsonData = JsonUtility.ToJson(data);
+        StartCoroutine(PostRequest(URL, jsonData));
+
     }
+
+    
 
 }
